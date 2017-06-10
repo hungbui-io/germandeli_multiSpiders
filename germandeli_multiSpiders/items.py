e932aeb2c4f61994b15a5b68edbf248aa631d704
hungbv11 <hungbv88@gmail.com>
2017-06-10 21:54:18 +0200

    adding stuff

--- germandeli_multiSpiders/items.py
+++ germandeli_multiSpiders/items.py
@@ -9,6 +9,9 @@ import scrapy
 
 
 class GermandeliMultispidersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    price = scrapy.Field()
+    description = scrapy.Field()
+    ingridients = scrapy.Field()
+    image = scrapy.Field()
+    image_url = scrapy.Field()
