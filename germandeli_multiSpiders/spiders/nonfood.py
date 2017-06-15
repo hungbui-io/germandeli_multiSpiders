4645af41db1732ca11afdb4f752ed9b20bbb25f8
hungbv11 <hungbv88@gmail.com>
2017-06-15 11:26:13 +0200

    new structure for nonfood

--- germandeli_multiSpiders/spiders/nonfood.py
+++ germandeli_multiSpiders/spiders/nonfood.py
@@ -9,14 +9,32 @@ from datetime import date
 class NonfoodSpider(scrapy.Spider):
     name = "nonfood"
     allowed_domains = ["germandeli.com"]
-    start_urls = ['http://germandeli.com/NonFood']
+    start_urls = ['http://germandeli.com/NonFood',
+                  'http://www.germandeli.com/NonFood/Aprons_and_T-Shirts',
+                  'http://www.germandeli.com/NonFood/Aprons_and_T-Shirts/German__Aprons',
+                  'http://www.germandeli.com/NonFood/Buecher_Romane_Raetsel_uzw',
+                  'http://www.germandeli.com/NonFood/Buecher_Romane_Raetsel_uzw/Cookbooks',
+                  'http://www.germandeli.com/NonFood/Buecher_Romane_Raetsel_uzw/Cookbooks/Cookbooks_English_Language',
+                  'http://www.germandeli.com/NonFood/Buecher_Romane_Raetsel_uzw/Cookbooks/Cookbooks_German_Language',
+                  'http://www.germandeli.com/NonFood/Buecher_Romane_Raetsel_uzw/Kinder-Buecher',
+                  'http://www.germandeli.com/NonFood/Buecher_Romane_Raetsel_uzw/Romane/Sammelbaende_2',
+                  'http://www.germandeli.com/NonFood/Buecher_Romane_Raetsel_uzw/Romane/Single_Romane',
+                  'http://www.germandeli.com/NonFood/Brooms_Brushes_Cloths',
+                  'http://www.germandeli.com/NonFood/DetergentsCleaners',
+                  'http://www.germandeli.com/NonFood/DetergentsCleaners/Gardinen_NeuCurtain_Cleansers',
+                  'http://www.germandeli.com/NonFood/DetergentsCleaners/Miscellaneous_Cleansers',
+                  'http://www.germandeli.com/NonFood/DetergentsCleaners/WaschmittelWeichspueler_Laundry_Helpers',
+                  'http://www.germandeli.com/NonFood/Housewares',
+                  'http://www.germandeli.com/NonFood/Housewares/Barware',
+                  'http://www.germandeli.com/NonFood/MagazinesZeitschriften',
+                  'http://www.germandeli.com/NonFood/Napkins_and_Paper_Goods', ]
     custom_settings = {'FILES_STORE': '/home/hung/Projects/germandeli_multiSpiders/output/nonfood'}
 
     def parse(self, response):
-        urls = response.xpath('*//div[@class="category-cell-name"]/a/@href')
+        urls = response.xpath('*//ul[@class="nav nav-list"]/li/h4/a/@href')
         for url in urls:
-            yield scrapy.Request("http://www.germandeli.com/"+str(url.extract()), self.parse_page)
-            print(url)
+            yield scrapy.Request("http://www.germandeli.com"+str(url.extract()), self.parse_page)
+            print(url.extract())
 
     def parse_page(self, response):
         urls = response.xpath('*//h2[@class="item-cell-name"]/a/@href')
@@ -35,7 +53,7 @@ class NonfoodSpider(scrapy.Spider):
                                 # splash_url='<url>',  # optional; overrides SPLASH_URL
                                 # slot_policy=scrapy_splash.SlotPolicy.PER_DOMAIN,  # optional
                                 )
-            #print(url)
+            print(url.extract())
 
         next = response.xpath('*//div[@class="pagination pagination-small pull-right"]/ul/li[3]/a/@href')
         yield SplashRequest("http://www.germandeli.com" + str(next.extract_first()), self.parse_page,
@@ -53,7 +71,7 @@ class NonfoodSpider(scrapy.Spider):
                                 # slot_policy=scrapy_splash.SlotPolicy.PER_DOMAIN,  # optional
                                 )
 
-        #print(next)
+        print(next.extract_first)
 
 
     def parse_product(self, response):
