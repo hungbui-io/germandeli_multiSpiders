1e4c2deb28cc540014e04be63743f8ed7b7ae59f
hungbv11 <hungbv88@gmail.com>
2017-06-23 18:10:03 +0200

    grouping nonfood categories

--- germandeli_multiSpiders/spiders/nonfood_3.py
+++ germandeli_multiSpiders/spiders/nonfood_3.py
@@ -9,7 +9,11 @@ from datetime import date
 class SeasonalSpider(scrapy.Spider):
     name = "nonfood3"
     allowed_domains = ["germandeli.com"]
-    start_urls = ['http://www.germandeli.com/NonFood/MagazinesZeitschriften',]
+    start_urls = ['http://www.germandeli.com/NonFood/MagazinesZeitschriften',
+                  'http://www.germandeli.com/NonFood/Napkins_and_Paper_Goods',
+                  'http://www.germandeli.com/NonFood/Brooms_Brushes_Cloths',
+                  'http://www.germandeli.com/NonFood/DetergentsCleaners',
+                  ]
     custom_settings = {'FILES_STORE': '/home/hung/Projects/germandeli_multiSpiders/output/nonfood'}
 
     def parse(self, response):
