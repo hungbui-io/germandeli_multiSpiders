cd9565eeddc2a3b52e2e0645601f68b9221b2bff
Hung Bui <hungbv88@gmail.com>
2017-08-14 11:42:54 +0200

    typo

--- germandeli_multiSpiders/pipelines.py
+++ germandeli_multiSpiders/pipelines.py
@@ -14,7 +14,7 @@ from scrapy.exporters import JsonItemExporter, CsvItemExporter, XmlItemExporter
 import json
 import codecs
 
-class GermandeliPipeline(object):
+class GermandeliMultispiders(object):
     def process_item(self, item, spider):
         return item
 
